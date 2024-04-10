4343d3c184ad54d6e7df853f0d4316f630e9813e
AE06909 <wes.gonzales@elevancehealth.com>
2024-04-10 12:48:31 -0400

    Sample project complete.

--- src/hooks/useTitle.js
+++ src/hooks/useTitle.js
@@ -1,7 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
-export const useTitle = () => {
-  return (
-    <div>useTitle</div>
-  )
+export const useTitle = (title) => {
+  useEffect(() => {
+    document.title =`${title} | Shopping Cart`
+  },[title])
+  return null
 }
